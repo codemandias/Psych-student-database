281961a5239e4f9d6e6c7b20103966949d85a37b
sahib <sh297668@dal.ca>
2021-02-24 12:32:35 -0400

    created the website file structure

---
+++ 3691 website interface/includes/header.php
@@ -0,0 +1,20 @@
+<?php echo "
+<head>
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <link rel='stylesheet' href='css/styles.css'>
+    <title>Psychology Graduate Database</title>
+</head>
+<body>
+<header>
+    <nav>
+        <div>
+            <input type='image' src='images/dalLogo.jpg' height='100px'  onclick='location.href=\"index.php\"'/>
+            <h1 class='navbar_Header'>Dalhousie University Psychology Graduate Database</h1>
+        </div>
+        <hr class='navbar_Hr'/>
+    </nav>
+    
+</header>
+</body>
+</html>";
