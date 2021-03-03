d2cfbedb72c4cb15b0e838fb0d41c57ca976a6b4
fernander <sr529182@dal.ca>
2021-03-03 14:23:13 -0400

    Changes made to stylesheet and header as well as edits for calculations.php

--- 3691 website interface/calculations.php
+++ 3691 website interface/calculations.php
@@ -1,5 +1,20 @@
 <?php
-    $parentFile = "Calculations"; //$parentFile is used to find what file called includes header so header cna be set to file name. DO NOT EDIT
-    $pageDir = basename(__DIR__);
-    require 'includes/header.php';
+$parentFile = "Calculations"; //$parentFile is used to find what file called includes header so header cna be set to file name. DO NOT EDIT
+$pageDir = basename(__DIR__);
+require 'includes/header.php';
 ?>
+<body>
+
+<div class="sidebar">
+    <a class="active" href="#Database-Entry">Scholarship</a>
+    <a href=''>Faculty</a>
+    <a href=''>Misc</a>
+</div>
+
+<div id="content">
+
+</div>
+<?php
+require 'includes/footer.php';
+?>
+</body>
