ac4cb6d05df0bf24ed3d1597bd412eedc1aace99
Aditya Sharma <ad437951@dal.ca>
2021-03-03 13:12:44 -0400

    solved issue with forms in database-entry, admission and awards page

--- 3691 website interface/includes/award.php
+++ 3691 website interface/includes/award.php
@@ -13,11 +13,10 @@
             <a href="#Progress">Progress</a>
             <a href="#Status">Status</a>
         </div>
-
-        <div id="content">
+            <form method="get" action="">
+            <div class="content">
             <div class = "contentLeft">
                 <h3>Student Information</h3>
-                <form method="get" action="">
                     <p id = "row">
                         <label for="sInfo">*Student(First/Last): </label>
                             <input type="text" id="nameField" name="firstName" required >
