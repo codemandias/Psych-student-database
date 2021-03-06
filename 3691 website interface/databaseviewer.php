2672774019d670699ae3db3424aed47a92f27ea7
Jacob Coyne <jc910209@dal.ca>
2021-03-05 17:21:14 -0700

    Added button filter functionality

--- 3691 website interface/databaseviewer.php
+++ 3691 website interface/databaseviewer.php
@@ -6,15 +6,15 @@ require 'includes/header.php';
 
 <body>
     <div class="sidebar">
-      <form>
-        <input type="submit" name="show_all_student" value="Show all students"/>
-        <input type="submit" name="show_by_semmester" value="Show by semester"/>
-        <input type="submit" name="show_by_program" value="Show by program"/>
-        <input type="submit" name="show_by_degree" value="Show by degree"/>
-      </form>
+        <form method="post">
+            <input type="submit" name="show_all_student" value="Show all students" />
+            <input type="submit" name="show_by_semmester" value="Show by semester" />
+            <input type="submit" name="show_by_program" value="Show by program" />
+            <input type="submit" name="show_by_degree" value="Show by degree" />
+        </form>
     </div>
     <div class="contentTable">
-        <input type="text" id="search" name="search" placeholder="search" required >
+        <input type="text" id="search" name="search" placeholder="search" required>
         <table class="mytable">
             <tr>
                 <th>Student Name</th>
@@ -23,7 +23,29 @@ require 'includes/header.php';
                 <th>Program</th>
             </tr>
             <?php
-            $studentsQuery = "SELECT * FROM `student`";
+            //if show all student button is pressed display all students
+            if (isset($_POST["show_all_student"])) {
+                $studentsQuery = "SELECT * FROM `student` ORDER BY `student`.`last_name`";
+            }
+            //else if show by semester is pressed display current students
+            else if (isset($_POST["Show by semester"])) {
+                "SELECT * FROM `student` 
+				 INNER JOIN `student_status` 
+                 ON `student`.`student_id` =  `student_status`.`student_id` 
+				 WHERE `student_status`.`program_status_today` = 'Current' ";
+            }
+            //else if show by program is pressed organize students by program name
+            else if (isset($_POST["show_by_program"])) {
+                $studentsQuery = "SELECT * FROM `student` ORDER BY `student`.`program_type`";
+            }
+            //else if show by degree is pressed organize students by degree name
+            else if (isset($_POST["show_by_degree"])) {
+                $studentsQuery = "SELECT * FROM `student` ORDER BY `student`.`degree_type`";
+            }
+            //if no button has been pressed just display all students
+            else {
+                $studentsQuery = "SELECT * FROM `student`";
+            }
             //query result
             $result = $dbconnection->query($studentsQuery);
 
